cbea34d6c0d622ff7b07d54cd6742bd52bfefa55
W L <weidong_ca00@yahoo.com>
2021-01-12 17:19:31 -0600

    update

---
+++ frontend/test.js
@@ -0,0 +1,31 @@
+function changefont()
+{
+
+    var x = document.getElementById("cn");
+    x.style.fontSize = "25px";           
+    x.style.color = "red"; 
+}
+
+function setupCarousel(){
+    $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 5,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 2,
+        nav: false
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: true,
+        margin: 5
+      }
+    }
+  })
+}
